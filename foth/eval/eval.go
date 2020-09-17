906c9e5aba562463f25a0cde2b935b306f192479
Steve Kemp <steve@steve.org.uk>
2020-09-17 17:43:36 +0300

    Allow get/set of variables
        > variable x
        > 93.1 x !
        > x @ .
        93.1

--- foth/eval/eval.go
+++ foth/eval/eval.go
@@ -11,6 +11,15 @@ import (
 	"foth/stack"
 )
 
+// Variable is the structure for storing variable names, and contents
+type Variable struct {
+	// Name is the name of the variable
+	Name string
+
+	// Value is the value we store within it.
+	Value float64
+}
+
 // Word is the structure for a single word.
 type Word struct {
 	// Name is the name of the function "+", "print", etc.
@@ -48,6 +57,8 @@ type Eval struct {
 	// Dictionary entries
 	Dictionary []Word
 
+	// Private details
+
 	// Literal strings.  As encountered in our program.
 	strings []string
 
@@ -73,6 +84,12 @@ type Eval struct {
 	// Here we keep track of them
 	ifOffset1 int
 	ifOffset2 int
+
+	// Variables
+	vars []Variable
+
+	// Are we currently defining a variable?
+	defining bool
 }
 
 // New returns a simple evaluator, which will allow executing forth-like words.
@@ -88,7 +105,6 @@ func New() *Eval {
 
 	// Populate our built-in functions.
 	e.Dictionary = []Word{
-		Word{Name: "#words", Function: e.wordLen},
 		Word{Name: "*", Function: e.mul},
 		Word{Name: "+", Function: e.add},
 		Word{Name: "-", Function: e.sub},
@@ -103,8 +119,6 @@ func New() *Eval {
 		Word{Name: "==", Function: e.eq},
 		Word{Name: ">", Function: e.gt},
 		Word{Name: ">=", Function: e.gtEq},
-		Word{Name: "debug", Function: e.debugSet},
-		Word{Name: "debug?", Function: e.debugp},
 		Word{Name: "do", Function: e.nop, StartImmediate: true},
 		Word{Name: "drop", Function: e.drop},
 		Word{Name: "dump", Function: e.dump},
@@ -118,7 +132,19 @@ func New() *Eval {
 		Word{Name: "print", Function: e.print},
 		Word{Name: "swap", Function: e.swap},
 		Word{Name: "then", Function: e.nop, EndImmediate: true},
+
+		// debug-handling
+		Word{Name: "debug", Function: e.debugSet},
+		Word{Name: "debug?", Function: e.debugp},
+
+		// word-handling
+		Word{Name: "#words", Function: e.wordLen},
 		Word{Name: "words", Function: e.words},
+
+		// variable-handling
+		Word{Name: "!", Function: e.setVar},
+		Word{Name: "@", Function: e.getVar},
+		Word{Name: "variable", Function: e.variable},
 	}
 
 	return e
@@ -158,10 +184,13 @@ func (e *Eval) Eval(input string) error {
 		// in the case of string-literals
 		tok := token.Name
 
-		// Trim the leading/trailing spaces,
-		// and skip any empty tokens
-		tok = strings.TrimSpace(tok)
-		if tok == "" {
+		// Are we defining a variable?
+		if e.defining {
+			if e.debug {
+				fmt.Printf("defining variable %s\n", tok)
+			}
+			e.vars = append(e.vars, Variable{Name: tok})
+			e.defining = false
 			continue
 		}
 
@@ -204,6 +233,13 @@ func (e *Eval) Eval(input string) error {
 				}
 			}
 		} else {
+
+			// Is this a variable?  If so push the variable offset
+			idx = e.findVariable(tok)
+			if idx >= 0 {
+				e.Stack.Push(float64(idx))
+				continue
+			}
 
 			// If we didn't handle this as a word, then
 			// assume it is a number.
@@ -419,6 +455,17 @@ func (e *Eval) compileToken(token lexer.Token) error {
 		return nil
 	}
 
+	// OK so we're compiling something - and it wasn't a word
+	// was it a variable?
+	idx = e.findVariable(tok)
+	if idx >= 0 {
+		// compile this into something that will push
+		// the offset of the variable onto the stack
+		e.tmp.Words = append(e.tmp.Words, -1)
+		e.tmp.Words = append(e.tmp.Words, float64(idx))
+		return nil
+	}
+
 	// At this point we assume the user entered a number
 	// so we save a magic "-1" flag in our
 	// definition, and then the number itself
@@ -610,8 +657,9 @@ func (e *Eval) evalWord(index int) error {
 	return nil
 }
 
-// findWords returns the index in our dictionary of the entry for the
-// given-name.  Returns -1 if the word cannot be found.
+// findWords returns the index of the specified word in our dictionary.
+//
+// Returns -1 if the word cannot be found.
 func (e *Eval) findWord(name string) int {
 	for index, word := range e.Dictionary {
 		if name == word.Name {
@@ -682,4 +730,21 @@ func (e *Eval) printString(str string) {
 	str = strings.ReplaceAll(str, "\\t", "\t")
 	str = strings.ReplaceAll(str, "\\r", "\r")
 	fmt.Printf("%s", str)
+}
+
+// findVariable returns the index of the specified variable in our list
+// of variables.
+//
+// Returns -1 if the variable cannot be found.
+//
+// Yes we store these in an array, rather than a map.  That's because
+// we want to differentiate between an undefined variable and one with
+// no value.
+func (e *Eval) findVariable(name string) int {
+	for i, v := range e.vars {
+		if v.Name == name {
+			return i
+		}
+	}
+	return -1
 }
