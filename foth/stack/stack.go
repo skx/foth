dd0cc85bf9ba69e9345ac1b138afa2b8050fa47b
Steve Kemp <steve@steve.org.uk>
2020-09-19 18:34:55 +0300

    Comment-updates

--- foth/stack/stack.go
+++ foth/stack/stack.go
@@ -8,27 +8,27 @@ import (
 // Stack holds our numbers.
 type Stack []float64
 
-// At shows the value at the given offset
+// At returns the value at the given offset.
 func (s *Stack) At(offset int) float64 {
 	return (*s)[offset]
 }
 
-// IsEmpty checks if our stack is empty.
+// IsEmpty checks whether the stack is empty.
 func (s *Stack) IsEmpty() bool {
 	return len(*s) == 0
 }
 
-// Len returns the length of the stack
+// Len returns the length of the stack.
 func (s *Stack) Len() int {
 	return len(*s)
 }
 
-// Push adds a new number to the stack
+// Push adds a new number to the top of the stack.
 func (s *Stack) Push(x float64) {
 	*s = append(*s, x)
 }
 
-// Pop removes and returns the top element of stack.
+// Pop removes, and returns, the top element of stack.
 func (s *Stack) Pop() (float64, error) {
 	if s.IsEmpty() {
 		return 0, fmt.Errorf("stack underflow")
